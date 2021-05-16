4ac36bb2fc201f98dd80046d7f9b62621874bd59
developer <developer@ITWORKSs-MacBook-Pro.local>
2021-05-16 12:26:58 +0530

    show table after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
     # byebug
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      if user.is_active
+        session[:user_id] = user.id
+        redirect_to root_url, notice: "Logged in!"
+      else
+        flash.now[:alert] = "Your Account Not Activeted Yet."
+        render "new"
+      end
     else
       flash.now[:alert] = "Email or password is invalid"
       render "new"
